3d4f2e3279df7227a7ac80912366ba933ba1f87e
lulu73211 <99766941+lulu73211@users.noreply.github.com>
2024-05-31 11:11:40 +0200

    projetjo

--- src/main/java/com/tp/olympics/repository/StadiumRepository.java
+++
@@ -1,7 +0,0 @@
-package com.tp.olympics.repository;
-
-import com.tp.olympics.entity.Stadium;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface StadiumRepository extends JpaRepository<Stadium, Long> {
-}
